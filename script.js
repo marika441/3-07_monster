921932343c9d9303832369a54a4b675de64c249e
marika441 <92457238+marika441@users.noreply.github.com>
2022-02-03 17:08:28 +0900

    [Finish]3-7回数が決まっていない繰り返し

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let enemy = 100;
+let count = 0;
+
+window.alert('戦闘スタート！');
+while(enemy > 0) {
+  const attack = Math.floor(Math.random() * 30) + 1;
+  console.log('モンスターに' + attack + 'のダメージ！');
+  // enemy = enemy - attack;
+  enemy -= attack;
+  count += 1;
+}
+console.log(count + '回でモンスターを倒した！');
